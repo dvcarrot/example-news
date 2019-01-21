bdf5bfb191dfb1a65cb3011b7684d94b9e413b1f
dvcarrot <dvcarrot@yandex.ru>
2019-01-21 11:14:59 +0300

    it add slider structure for news and styles for desktop visibility

--- src/scripts/ctrl/news.js
+++ src/scripts/ctrl/news.js
@@ -1,6 +1,19 @@
 // news controller
 
 function news() {
-    console.log('news here');
-    console.log(this);
+
+    // globals
+    var TABLET_BREAKPOINT = Constants.TABLET_BREAKPOINT;
+    var DESKTOP_BREAKPOINT = Constants.DESKTOP_BREAKPOINT;
+
+    // responsive options
+    var responsiveOptions = {};
+    responsiveOptions[TABLET_BREAKPOINT] = {items: 2};
+    responsiveOptions[DESKTOP_BREAKPOINT] = {disable: true};
+
+    // slider options
+    var options = {container: this, mouseDrag: true, items: 1, gutter: 50, responsive: responsiveOptions, navPosition: 'bottom', controls: false};
+
+    // init slider
+    tns(options);
 }
